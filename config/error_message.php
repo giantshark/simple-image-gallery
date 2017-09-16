6109c6f4654b68b16628466aaee0351de6add442
dev <dev@local.com>
2017-09-16 20:24:02 +0700

    Add error message invalid image id

--- config/error_message.php
+++ config/error_message.php
@@ -2,5 +2,6 @@
 
 return [
     "upload_image_not_success" => ["en" => "upload image not success", "th" => "upload image not success"],
-    "image_not_found" => ["en" => "image not found", "th" => "image not found"]
+    "image_not_found" => ["en" => "image not found", "th" => "image not found"],
+    "invalid_image_id" => ["en" => "invalid image id", "th" => "invalid image id"],
 ];
