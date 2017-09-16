3c48ee3f16dd5716a2c578a9fdef68eedb007cf9
dev <dev@local.com>
2017-09-16 15:15:29 +0700

    add image not found error message

--- config/error_message.php
+++ config/error_message.php
@@ -1,5 +1,6 @@
 <?php
 
 return [
-    "upload_image_not_success" => ["en" => "upload image not success", "th" => "upload image not success"]
+    "upload_image_not_success" => ["en" => "upload image not success", "th" => "upload image not success"],
+    "image_not_found" => ["en" => "image not found", "th" => "image not found"]
 ];
