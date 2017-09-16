0ba437d740b2eb030e514a9374e36ffe8dd44922
dev <dev@local.com>
2017-09-16 19:20:20 +0700

    move image

---
+++ tests/Feature/FileUploadTest.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Tests;
+
+use Tests\TestCase;
+use App\Container\Authentication\Models\User;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Http\UploadedFile;
+
+class FileUploadTest extends TestCase
+{
+
+    use RefreshDatabase;
+
+    public function testMustListIamges()
+    {
+        $user = factory(User::class)->create();
+        $response = $this->actingAs($user)->call('GET', 'api/galleries');
+        $data = $this->parseJson($response);
+        $this->assertEquals(200, $data->code);
+    }
+
+    public function testUploadImageMustSuccess()
+    {
+        $user = factory(User::class)->create();
+        $response = $this->actingAs($user)->call('POST', 'api/galleries', [
+            'file' => UploadedFile::fake()->image('avatar.jpg')
+        ]);
+        $data = $this->parseJson($response);
+        $this->assertEquals(200, $data->code);
+        $this->assertEquals('avatar.jpg', $data->response[0]->original_name);
+    }
+
+    public function testUploadImageMustNotSuccess()
+    {
+        $user = factory(User::class)->create();
+        $response = $this->actingAs($user)->call('POST', 'api/galleries', [
+            'file' => UploadedFile::fake()->image('avatar.svg')
+        ]);
+        $data = $this->parseJson($response);
+        $this->assertEquals(400, $data->code);
+        $this->assertEquals('The file must be a file of type: image/jpeg, image/png.', $data->message[0]);
+    }
+
+    public function testDeleteImageMustSuccess()
+    {
+        $user = factory(User::class)->create();
+        $uploadResponse = $this->actingAs($user)->call('POST', 'api/galleries', [
+            'file' => UploadedFile::fake()->image('avatar.png')
+        ]);
+        $uploadResponseData = $this->parseJson($uploadResponse);
+        $response = $this->actingAs($user)->call('DELETE', 'api/galleries/'.$uploadResponseData->response[0]->id);
+        $data = $this->parseJson($response);
+        $this->assertEquals(200, $data->code);
+    }
+
+    public function testDeleteImageMustNotSuccess()
+    {
+        $user = factory(User::class)->create();
+        $response = $this->actingAs($user)->call('DELETE', 'api/galleries/200');
+        $data = $this->parseJson($response);
+        $this->assertEquals(400, $data->code);
+        $this->assertEquals('image not found', $data->message[0]);
+    }
+
+}
---
+++ tests/Unit/DiskUsageTest.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Container\DiskManagement\Classes\Disk;
+use App\Container\Gallery\Models\Gallery;
+use Tests\TestCase;
+use App\Container\Authentication\Models\User;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Http\UploadedFile;
+
+class DiskUsageTest extends TestCase
+{
+
+    use RefreshDatabase;
+
+    public function testDiskUsageMustIncreaseByImageSize()
+    {
+        $user = factory(User::class)->create();
+        $uploadResponse = $this->actingAs($user)->call('POST', 'api/galleries', [
+            'file' => UploadedFile::fake()->image('avatar.png')
+        ]);
+        $uploadResponseData = $this->parseJson($uploadResponse);
+        $disk = new Disk(new Gallery());
+        $this->assertEquals($uploadResponseData->response[0]->size, $disk->getSizeKb());
+        $this->assertEquals(1, $disk->getToTalFile());
+    }
+
+    public function testTotalDiskUsageMustCorrect()
+    {
+        $totalSize = 0;
+        for ($i = 0; $i < 4; $i++) {
+            $user = factory(User::class)->create();
+            $uploadResponse = $this->actingAs($user)->call('POST', 'api/galleries', [
+                'file' => UploadedFile::fake()->image('avatar.png')
+            ]);
+            $uploadResponseData = $this->parseJson($uploadResponse);
+            $totalSize += $uploadResponseData->response[0]->size;
+        }
+        $disk = new Disk(new Gallery());
+        $this->assertEquals($totalSize, $disk->getSizeKb());
+        $this->assertEquals(4, $disk->getToTalFile());
+    }
+
+}
